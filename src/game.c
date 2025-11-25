bf149886ec319d429986bfddee41a84834a56149
Axel Lindberg <axel6@kth.se>
2025-11-25 11:40:32 +0100

    Updateted fill function

--- src/game.c
+++ src/game.c
@@ -5,19 +5,22 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define FRAMEBUFFER_ADDRESS ((volatile char*)0x08000000)
+
 static char is_running = 1;
 
 void game_set_running_state(char state) {
 	is_running = state;
 }
 
+void fill_background(int color) {
+    for (int i = 0; i < 320*240; i++)
+        FRAMEBUFFER_ADDRESS[i] = color; 
+}
 
 void game_init() {
-	
-	volatile char *VGA = (volatile char*)0x08000000;
+	fill_background(80);
 
-    for (int i = 0; i < 320*240; i++)
-        VGA[i] = 80; 
 }
 
 void game_run() {
