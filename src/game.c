c54ecf705e4211f20e86a30cdd7104c47f940a36
Axel Lindberg <axel6@kth.se>
2025-11-25 11:31:49 +0100

    Added green background

--- src/game.c
+++ src/game.c
@@ -2,15 +2,22 @@
 #include "dtekv-lib.h"
 
 #include "assets/test.txt.h"
+#include <stdint.h>
+#include <stddef.h>
 
-char is_running = 1;
+static char is_running = 1;
 
 void game_set_running_state(char state) {
 	is_running = state;
 }
 
-void game_init() {
 
+void game_init() {
+	
+	volatile char *VGA = (volatile char*)0x08000000;
+
+    for (int i = 0; i < 320*240; i++)
+        VGA[i] = 80; 
 }
 
 void game_run() {
