4a4a8b8b8b06a534722d9dbaae739280c4b83bf0
YayL <jonasbeast9@gmail.com>
2025-12-05 15:16:07 +0100

    Changed player_draw to just draw player
    Made a small change to player_move to just lookup needed blocks

--- src/menus/playing/player.c
+++ src/menus/playing/player.c
@@ -32,34 +32,40 @@ static inline void player_move(int32_t dx, int32_t dy) {
 
 	// Unsure why the -1 are neccessary but they fixed an ¿issue?
 
-	const char top_left_dx = !IS_AVAILABLE(next_x, player.position.y);
-	const char top_right_dx = !IS_AVAILABLE(next_x + PLAYER_WIDTH - 1, player.position.y);
-	const char bottom_left_dx = !IS_AVAILABLE(next_x, player.position.y + PLAYER_HEIGHT - 1);
-	const char bottom_right_dx = !IS_AVAILABLE(next_x + PLAYER_WIDTH - 1, player.position.y + PLAYER_HEIGHT - 1);
-
-	if (dx < 0 && (top_left_dx || bottom_left_dx)) {
-		// Going left and top left or bottom left intersects
-		next_x = GRID_X_TO_SCREEN((SCREEN_X_TO_GRID(next_x) + 1));
-	} else if (dx > 0 && (top_right_dx || bottom_right_dx)) {
-		// Going right and top right or bottom right intersects
-		next_x = GRID_X_TO_SCREEN(SCREEN_X_TO_GRID(next_x + PLAYER_WIDTH)) - PLAYER_WIDTH;
+	if (dx < 0) {
+		const char top_left_dx = !IS_AVAILABLE(next_x, player.position.y);
+		const char bottom_left_dx = !IS_AVAILABLE(next_x, player.position.y + PLAYER_HEIGHT - 1);
+		if (top_left_dx || bottom_left_dx) {
+			// Going left and top left or bottom left intersects
+			next_x = GRID_X_TO_SCREEN((SCREEN_X_TO_GRID(next_x) + 1));
+		}
+	} else if (dx > 0) {
+		const char top_right_dx = !IS_AVAILABLE(next_x + PLAYER_WIDTH - 1, player.position.y);
+		const char bottom_right_dx = !IS_AVAILABLE(next_x + PLAYER_WIDTH - 1, player.position.y + PLAYER_HEIGHT - 1);
+		if (top_right_dx || bottom_right_dx) {
+			// Going right and top right or bottom right intersects
+			next_x = GRID_X_TO_SCREEN(SCREEN_X_TO_GRID(next_x + PLAYER_WIDTH)) - PLAYER_WIDTH;
+		}
 	}
 
-	const char top_left_dy = !IS_AVAILABLE(player.position.x, next_y);
-	const char top_right_dy = !IS_AVAILABLE(player.position.x + PLAYER_WIDTH - 1, next_y);
-	const char bottom_left_dy = !IS_AVAILABLE(player.position.x, next_y + PLAYER_HEIGHT - 1);
-	const char bottom_right_dy = !IS_AVAILABLE(player.position.x + PLAYER_WIDTH - 1, next_y + PLAYER_HEIGHT - 1);
 
-	if (dy < 0 && (top_left_dy || top_right_dy)) {
-		// Going up and top left or top right intersects
-		next_y = GRID_Y_TO_SCREEN((SCREEN_Y_TO_GRID(next_y) + 1));
-	} else if (dy > 0 && (bottom_left_dy || bottom_right_dy)) {
-		// Going down and bottom left or bottom right intersects
-		next_y = GRID_Y_TO_SCREEN(SCREEN_Y_TO_GRID(next_y + PLAYER_HEIGHT)) - PLAYER_HEIGHT;
+	if (dy < 0) {
+		const char top_left_dy = !IS_AVAILABLE(player.position.x, next_y);
+		const char top_right_dy = !IS_AVAILABLE(player.position.x + PLAYER_WIDTH - 1, next_y);
+		if (top_left_dy || top_right_dy) {
+			// Going up and top left or top right intersects
+			next_y = GRID_Y_TO_SCREEN((SCREEN_Y_TO_GRID(next_y) + 1));
+		}
+	} else if (dy > 0) {
+		const char bottom_left_dy = !IS_AVAILABLE(player.position.x, next_y + PLAYER_HEIGHT - 1);
+		const char bottom_right_dy = !IS_AVAILABLE(player.position.x + PLAYER_WIDTH - 1, next_y + PLAYER_HEIGHT - 1);
+		if (bottom_left_dy || bottom_right_dy) {
+			// Going down and bottom left or bottom right intersects
+			next_y = GRID_Y_TO_SCREEN(SCREEN_Y_TO_GRID(next_y + PLAYER_HEIGHT)) - PLAYER_HEIGHT;
+		}
 	}
 
-	// Since we don't check dxdy it is most likely possible to glitch through a
-	// block by going diagonally through it
+	// Since we don't check dxdy it can start twiching in a block by going diagonally into it
 
 	player.position.x = next_x;
 	player.position.y = next_y;
@@ -106,13 +112,27 @@ void player_update(uint32_t delta) {
 }
 
 void player_draw() {
-	draw_texture(
-		player.position.x,
-		player.position.y,
-		PLAYER_TEXTURE
-	);
+	volatile unsigned char * const framebuffer = screen_get_framebuffer_addr();
+	const uint32_t player_x = player.position.x, player_y = player.position.y;
+
+	for (uint32_t y = 0; y < BLOCK_SIZE; ++y) {
+		for (uint32_t x = 0; x < BLOCK_SIZE; ++x) {
+			if (PLAYER_TEXTURE[y * BLOCK_SIZE+ x] == BACKGROUND_GREEN) {
+				continue;
+			}
+
+			framebuffer[SCREEN_TO_PIXEL(player_x + x, player_y + y)] = PLAYER_TEXTURE[y * BLOCK_SIZE + x];
+		}
+	}
 }
 
-struct player_position player_get_position() {
+struct position player_get_position() {
 	return player.position;
+}
+
+struct position player_get_grid_position() {
+	return (struct position) {
+		.x = SCREEN_X_TO_GRID(player.position.x),
+		.y = SCREEN_Y_TO_GRID(player.position.y)
+	};
 }
